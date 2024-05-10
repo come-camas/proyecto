890dd8d26465d5d64df2472b99331c840f839ed9
moises martinez <moisesalejandromar8@gmail.com>
2024-05-09 23:39:24 -0600

    cuñado enrrique

--- Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
+++ Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24fa6317fca57ebe313d504c7958f7ba2ec43e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c5189c824d9756c6eeac9abc05de59fc0d6774")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
