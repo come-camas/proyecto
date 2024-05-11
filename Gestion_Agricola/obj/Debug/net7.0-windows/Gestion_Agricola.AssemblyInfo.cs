f66d660d604195592f4b70a2093d683c714220fd
moises martinez <moisesalejandromar8@gmail.com>
2024-05-10 22:21:55 -0600

    prueba de gitflow

--- Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
+++ Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a83bce0340305e652643f47a3fad176c8336d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb414e49609e317c8528546d98ba9274b87bd71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
