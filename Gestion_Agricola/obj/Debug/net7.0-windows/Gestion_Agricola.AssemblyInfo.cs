36f49d73e01e6119c03934506a455f3c02728afc
Njean0405 <yireh.matagalpa@gmail.com>
2024-05-12 18:11:53 -0600

    se restablece

--- Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
+++ Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83b19540913524f5e6aee660e44ecc651709835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b25a8d744c1bfe6bf94f94c5a71e8a36eb6c8b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
