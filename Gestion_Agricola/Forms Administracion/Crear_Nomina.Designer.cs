1d05319ced00d18cb547f1115043f32a793028e6
moises martinez <moisesalejandromar8@gmail.com>
2024-05-24 11:48:19 -0600

    me la va a apelar yireh

--- Gestion_Agricola/Forms Administracion/Crear_Nomina.Designer.cs
+++ Gestion_Agricola/Forms Administracion/Crear_Nomina.Designer.cs
@@ -28,32 +28,381 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Crear_Nomina));
+            label2 = new Label();
             label1 = new Label();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            panel2 = new Panel();
+            label4 = new Label();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            pictureBox2 = new PictureBox();
+            textBox4 = new TextBox();
+            panel4 = new Panel();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            panel5 = new Panel();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            panel6 = new Panel();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            panel7 = new Panel();
+            label8 = new Label();
+            textBox8 = new TextBox();
+            panel8 = new Panel();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Codigo:";
             // 
             // label1
             // 
-            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(427, 318);
+            label1.BackColor = Color.FromArgb(35, 32, 39);
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(93, 126);
-            label1.TabIndex = 1;
-            label1.Text = "2";
+            label1.Size = new Size(1044, 51);
+            label1.TabIndex = 2;
+            label1.Text = "CREAR NOMINA";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.Location = new Point(12, 141);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 2);
+            panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(23, 21, 32);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(89, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(123, 24);
+            textBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(218, 110);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(37, 33);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 28);
+            label3.TabIndex = 7;
+            label3.Text = "Nombre:";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonFace;
+            panel2.Location = new Point(12, 203);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 2);
+            panel2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(12, 237);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 28);
+            label4.TabIndex = 9;
+            label4.Text = "Apellido:";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonFace;
+            panel3.Location = new Point(12, 268);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(250, 2);
+            panel3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(23, 21, 32);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(107, 176);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(148, 24);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(23, 21, 32);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Cursor = Cursors.IBeam;
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(107, 240);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(148, 24);
+            textBox3.TabIndex = 12;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(547, 110);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(37, 33);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 16;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(23, 21, 32);
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Cursor = Cursors.IBeam;
+            textBox4.ForeColor = Color.White;
+            textBox4.Location = new Point(418, 113);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(123, 24);
+            textBox4.TabIndex = 15;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ButtonFace;
+            panel4.Location = new Point(341, 141);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(200, 2);
+            panel4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(341, 110);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 28);
+            label5.TabIndex = 13;
+            label5.Text = "Cedula:";
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(23, 21, 32);
+            textBox5.BorderStyle = BorderStyle.None;
+            textBox5.Cursor = Cursors.IBeam;
+            textBox5.ForeColor = Color.White;
+            textBox5.Location = new Point(107, 307);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(148, 24);
+            textBox5.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.ButtonFace;
+            panel5.Location = new Point(12, 335);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(250, 2);
+            panel5.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(12, 304);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 28);
+            label6.TabIndex = 17;
+            label6.Text = "Direccion:";
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(23, 21, 32);
+            textBox6.BorderStyle = BorderStyle.None;
+            textBox6.Cursor = Cursors.IBeam;
+            textBox6.ForeColor = Color.White;
+            textBox6.Location = new Point(107, 374);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(148, 24);
+            textBox6.TabIndex = 22;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonFace;
+            panel6.Location = new Point(12, 402);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(250, 2);
+            panel6.TabIndex = 21;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(12, 371);
+            label7.Name = "label7";
+            label7.Size = new Size(90, 28);
+            label7.TabIndex = 20;
+            label7.Text = "Telefono:";
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.FromArgb(23, 21, 32);
+            textBox7.BorderStyle = BorderStyle.None;
+            textBox7.Cursor = Cursors.IBeam;
+            textBox7.ForeColor = Color.White;
+            textBox7.Location = new Point(107, 434);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(148, 24);
+            textBox7.TabIndex = 25;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.ButtonFace;
+            panel7.Location = new Point(12, 462);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(250, 2);
+            panel7.TabIndex = 24;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(12, 431);
+            label8.Name = "label8";
+            label8.Size = new Size(63, 28);
+            label8.TabIndex = 23;
+            label8.Text = "Email:";
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = Color.FromArgb(23, 21, 32);
+            textBox8.BorderStyle = BorderStyle.None;
+            textBox8.Cursor = Cursors.IBeam;
+            textBox8.ForeColor = Color.White;
+            textBox8.Location = new Point(161, 503);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(148, 24);
+            textBox8.TabIndex = 28;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = SystemColors.ButtonFace;
+            panel8.Location = new Point(12, 531);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(300, 2);
+            panel8.TabIndex = 27;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(12, 500);
+            label9.Name = "label9";
+            label9.Size = new Size(143, 28);
+            label9.TabIndex = 26;
+            label9.Text = "Departamento:";
             // 
             // Crear_Nomina
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(23, 21, 32);
             ClientSize = new Size(1044, 816);
+            Controls.Add(textBox8);
+            Controls.Add(panel8);
+            Controls.Add(label9);
+            Controls.Add(textBox7);
+            Controls.Add(panel7);
+            Controls.Add(label8);
+            Controls.Add(textBox6);
+            Controls.Add(panel6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(panel5);
+            Controls.Add(label6);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox4);
+            Controls.Add(panel4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(panel3);
+            Controls.Add(label4);
+            Controls.Add(panel2);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
             Controls.Add(label1);
+            Controls.Add(label2);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Crear_Nomina";
             Text = "Crear_Nomina";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
+        private Label label2;
         private Label label1;
+        private Panel panel1;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Panel panel2;
+        private Label label4;
+        private Panel panel3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private PictureBox pictureBox2;
+        private TextBox textBox4;
+        private Panel panel4;
+        private Label label5;
+        private TextBox textBox5;
+        private Panel panel5;
+        private Label label6;
+        private TextBox textBox6;
+        private Panel panel6;
+        private Label label7;
+        private TextBox textBox7;
+        private Panel panel7;
+        private Label label8;
+        private TextBox textBox8;
+        private Panel panel8;
+        private Label label9;
     }
 }
--- Gestion_Agricola/Forms Administracion/Crear_Nomina.cs
+++ Gestion_Agricola/Forms Administracion/Crear_Nomina.cs
@@ -16,5 +16,7 @@ namespace Gestion_Agricola
         {
             InitializeComponent();
         }
+
+
     }
 }
