6b60d30e8484a0017c995efc8676e102d72d0965
Njean0405 <yireh.matagalpa@gmail.com>
2024-07-04 19:24:07 -0600

    agregar estilos al form

--- Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
+++ Gestion_Agricola/obj/Debug/net7.0-windows/Gestion_Agricola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0429b085d8eb2bac943ded5714daf3663c3ff707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1102cbe4e7b74f111247ae303a373f7a07fb7ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion_Agricola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
